1517b71d648491114f21dcf1cb06cb912b84690d
Erik Castricum <git@cas-online.nl>
2019-10-31 08:19:58 +0100

    Revert "Fix build errors."
    This reverts commit a866d9a2f38bb771ee3b11ea110c23bfb702be1b.

--- hal/Android.mk
+++ hal/Android.mk
@@ -323,19 +323,6 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_BT_HAL)),true)
     LOCAL_SRC_FILES += audio_extn/bt_hal.c
 endif
 
-ifeq ($(strip $(USE_LIB_PROCESS_GROUP)),true)
-LOCAL_SHARED_LIBRARIES := \
-        liblog \
-        libcutils \
-        libtinyalsa \
-        libtinycompress_vendor \
-        libaudioroute \
-        libdl \
-        libaudioutils \
-        libexpat \
-        libhidltransport \
-        libprocessgroup
-else
 LOCAL_SHARED_LIBRARIES := \
 	liblog \
 	libcutils \
@@ -345,7 +332,6 @@ LOCAL_SHARED_LIBRARIES := \
 	libdl \
 	libaudioutils \
 	libexpat
-endif
 
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_QAP)),true)
 LOCAL_CFLAGS += -DQAP_EXTN_ENABLED -Wno-tautological-pointer-compare
